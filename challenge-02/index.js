f87e84ff3b09df72158dd03101f8d609264365c7
LeonardoCesca <leonardocescaf@gmail.com>
2020-07-11 12:19:58 -0300

    add: challenge 2

--- challenge-02/index.js
+++ challenge-02/index.js
@@ -1,27 +1,32 @@
 // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-
+const sum = (num1, num2) => {
+    return num1 + num2;
+}
 
 // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
-
+const receiveSum = sum(10, 1) + 5;
 
 // Qual o valor atualizado dessa variável
-
+console.log("O valor atualizado da variável eh: ", receiveSum);
 
 // Declare uma nova variável, sem valor.
-
+let variavel;
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
     O valor da variável agora é VALOR.
 Onde VALOR é o novo valor da variável.
 */
-
+const functionVariavel = () => {
+    return variavel = 'O valor da variável agora é VALOR.';
+}
 
 // Invoque a função criada acima.
-
+functionVariavel();
 
 // Qual o retorno da função (Use comentários de bloco).
 
+/* O valor da variável agora é VALOR. */
 
 /*
 Crie uma função com as seguintes características:
@@ -31,18 +36,26 @@ Crie uma função com as seguintes características:
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
 
+const funcaoTresArg = (arg1, arg2, arg3) => {
+    if (arg1 && arg2 && arg3) {
+        return (arg1 * arg2 * arg3) + 2;
+    } else {
+        return 'Preencha todos os valores corretamente!';
+    }
+}
 
 // Invoque a função criada acima, passando só dois números como argumento.
 
+funcaoTresArg();
 
 // Qual o resultado da invocação acima (Use comentários para mostrar o valor retornado).
-
+'Preencha todos os valores corretamente!'
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
-
+funcaoTresArg(1,2,3);
 
 // Qual o resultado da invocação acima (Use comentários para mostrar o valor retornado).
-
+8
 
 /*
 Crie uma função com as seguintes características:
@@ -54,5 +67,22 @@ Crie uma função com as seguintes características:
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
 
+const funcTresArgs = (arg1, arg2, arg3) => {
+    if (arg1 && arg2 && arg3) {
+        return (arg1 + arg2) / arg3;
+    } else if (!arg2 && !arg3) {
+        return arg1;
+    } else if (!arg3) {
+        return arg1 + arg2;
+    } else if (!arg1 && !arg2 && !arg3) {
+        return false;
+    }
+
+    return null;
+}
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+funcTresArgs();
+funcTresArgs(8);
+funcTresArgs(8,2);
+funcTresArgs(8, 2, 5);
